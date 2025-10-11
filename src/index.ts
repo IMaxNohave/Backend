4123c87b128d2eda24f70d470c6615a56e233237
Wachirawit Chaiyamat <karumanx003@gmail.com>
2025-10-11 10:57:07 +0700

    Revert "Merge pull request #3 from IMaxNohave/cleanupitem"

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import openapi from "@elysiajs/openapi";
 import { Elysia } from "elysia";
 import { auth } from "lib/auth";
 import { UserController } from "user/user.controller";
-import { scheduleDailyCleanup } from "./background/cleanupItems";
 
 const app = new Elysia()
 .mount(auth.handler) 
@@ -15,6 +14,3 @@ const app = new Elysia()
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
-
-// Start scheduled background jobs
-scheduleDailyCleanup();
