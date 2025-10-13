a19162b5c71f9ef987b87425bfcdb531c629244c
Ball <p.tum2168@gmail.com>
2025-10-14 03:09:22 +0700

    add dispute

--- src/order/constants.ts
+++ src/order/constants.ts
@@ -1,2 +1,3 @@
 export const HOLD_WINDOW_MS = 2.5 * 60 * 60 * 1000; // 2.5 ชั่วโมง (รอ seller accept)
 export const TRADE_WINDOW_MS = 1 * 60 * 60 * 1000; // 1 ชั่วโมง (หลัง accept)
+export const DISPUTE_EXTENSION_MS = 24 * 60 * 60 * 1000;
